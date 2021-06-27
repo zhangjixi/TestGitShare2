2223803af7a909610878e849ca9785867e359ccb
xin <30825274+zhangjixi@users.noreply.github.com>
2021-06-27 08:30:25 +0800

    dev 功能开发完成

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 20210627
         System.out.println("Helloorld!");
+
+        System.err.println("There is something wrong!");
     }
 }
